32904122ebea3171365f90b1e14a563f36e97fb9
Teoman Akçiçek <teomanakcicek39@gmail.com>
2017-03-03 12:50:58 +0200

    Update PowerOb.cpp

--- Hafta2/PowerOb.cpp
+++ Hafta2/PowerOb.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 
 using namespace std;
-
-int power(int a,int b){
-    int toplam=1;   //2
+int sayac=0;
+long long power(long long a,long long b){
+    long long toplam=1;   //2
     for(int i=0;i<b;i++){//2 1 2
+        sayac++;
         toplam*=a;      //2
     }
     return toplam;  //2+2+toplamsembolu b i=0 5 ten ihmallerden sonra O(n) cykty karmasyklyk
@@ -13,8 +14,8 @@ int power(int a,int b){
 
 int main()
 {
-    int sonuc=power(2,4);
-    cout<<sonuc;
+    long long sonuc=power(2,60);
+    cout<<sonuc<<" "<<sayac;
     system("PAUSE");
     return EXIT_SUCCESS;
 }
