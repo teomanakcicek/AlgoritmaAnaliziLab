7077f3603c01bc23597430a7052b01188aa32356
Teoman Akçiçek <teomanakcicek39@gmail.com>
2017-03-03 12:29:18 +0200

    Add files via upload

---
+++ PowerOb.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int power(int a,int b){
+    int toplam=1;   //2
+    for(int i=0;i<b;i++){//2 1 2
+        toplam*=a;      //2
+    }
+    return toplam;  //2+2+toplamsembolu b i=0 5 ten ihmallerden sonra O(n) cykty karmasyklyk
+}
+
+int main()
+{
+    int sonuc=power(2,4);
+    cout<<sonuc;
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
